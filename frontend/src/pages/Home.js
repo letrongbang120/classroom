e307238443ab41472efec4ad4ed4a82fa04dfee2
hoangdanhh16 <hoangdanhh16@gmail.com>
2021-11-19 21:09:32 +0700

    Design Login, Register, Home

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+import "./Home.css";
+
+function Home() {
+ 
+
+  return (
+    <div className="home">
+      <div className="home__container ">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/5/59/Google_Classroom_Logo.png"
+          alt="Google Classroom"
+          className="home__image"
+        />
+
+        <Link to="/login">
+            <button className="home__login w-100" >
+            Sign in
+            </button>
+        </Link>
+        
+
+        <Link to="/register" className='w-100'>
+            <button className="home__login w-100" >
+            Sign up
+            </button>
+        </Link>
+
+        <button className="home__login " >
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+
+
+  );
+}
+
+export default Home;
